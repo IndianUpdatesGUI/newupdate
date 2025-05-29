bc2e6898793bd3f5ffaf0a787b323362625b09cf
IndianUpdatesGUI <shreeramgourjhamar23@gmail.com>
2025-05-29 15:42:53 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,6 +1,7 @@
 // firebase-messaging-sw.js
-importScripts("https://www.gstatic.com/firebasejs/10.11.1/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/10.11.1/firebase-messaging-compat.js");
+
+importScripts("https://www.gstatic.com/firebasejs/10.11.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.11.0/firebase-messaging-compat.js");
 
 firebase.initializeApp({
   apiKey: "AIzaSyCgtGqEp7V8LAXtqyO3Ac2JZ4l8ESS4tjw",
@@ -8,15 +9,19 @@ firebase.initializeApp({
   projectId: "liveupdatepvtltd",
   storageBucket: "liveupdatepvtltd.appspot.com",
   messagingSenderId: "930997114628",
-  appId: "1:930997114628:web:29f886eedfd660c866e251"
+  appId: "1:930997114628:web:29f886eedfd660c866e251",
 });
 
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(function(payload) {
-  self.registration.showNotification(payload.notification.title, {
-    body: payload.notification.body,
-    icon: '/icon.png'
-  });
-});
+  console.log("[firebase-messaging-sw.js] Received background message", payload);
 
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: "/icon.png", // optional
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
