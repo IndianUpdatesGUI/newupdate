0cec624d217f685e3d7eab5e7291d7f7e401c618
IndianUpdatesGUI <shreeramgourjhamar23@gmail.com>
2025-06-08 15:48:25 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,28 +1,22 @@
-// Import Firebase scripts for messaging
+// firebase-messaging-sw.js
 importScripts('https://www.gstatic.com/firebasejs/10.11.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.11.0/firebase-messaging-compat.js');
 
-// Initialize Firebase
 firebase.initializeApp({
   apiKey: "AIzaSyCgtGqEp7V8LAXtqyO3Ac2JZ4l8ESS4tjw",
   authDomain: "liveupdatepvtltd.firebaseapp.com",
   projectId: "liveupdatepvtltd",
-  storageBucket: "liveupdatepvtltd.appspot.com",
   messagingSenderId: "930997114628",
   appId: "1:930997114628:web:29f886eedfd660c866e251"
 });
 
-// Initialize messaging
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(function(payload) {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
-    body: payload.notification.body,
-    icon: '/icon-192.png' // Update this path if needed
+    body: payload.notification.body
   };
-
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
