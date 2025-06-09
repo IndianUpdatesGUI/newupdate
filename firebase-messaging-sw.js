f4579da045f78475dbb35f373f0ba491b607d32d
IndianUpdatesGUI <shreeramgourjhamar23@gmail.com>
2025-06-09 15:01:27 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -38,3 +38,20 @@ const notificationOptions = {
 
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
+
+self.addEventListener('notificationclick', function(event) {
+  console.log("🔔 Notification clicked");
+  event.notification.close();
+
+  event.waitUntil(
+    clients.matchAll({ type: "window", includeUncontrolled: true }).then(function(clientList) {
+      for (const client of clientList) {
+        if (client.url === '/' && 'focus' in client)
+          return client.focus();
+      }
+      if (clients.openWindow)
+        return clients.openWindow('/');
+    })
+  );
+});
+
