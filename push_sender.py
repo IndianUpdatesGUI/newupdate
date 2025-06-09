54248174a75a7dab0eb7ab5f284d67ddb85fa422
IndianUpdatesGUI <shreeramgourjhamar23@gmail.com>
2025-06-09 15:10:30 +0530

    Update push_sender.py

--- push_sender.py
+++ push_sender.py
@@ -5,7 +5,7 @@ def SendPushNotification(server_key, device_token, title, message):
     url = 'https://fcm.googleapis.com/fcm/send'
 
     headers = {
-        'Authorization': 'key=BJLPfJ6DFU-ig_k_of9HA8H2wI6bWxe00bsmwl2_BHIIPyJZbHE5r0WWVrpmupATAtGMk4dKqnBpHoRxrR3i49M',
+        'Authorization': 'key=BH29N_jZscPwodEIDT0aC7o9l_WGmepP5cS31BViAYrG0xGaPrryFpA_zx7z0eLnJc1zTs0FihVttH2a3J5AihM',
 
         'Content-Type': 'application/json'
     }
@@ -27,7 +27,7 @@ def SendPushNotification(server_key, device_token, title, message):
 
 # Example call to the function — this is where the magic begins
 
-server_key = 'BJLPfJ6DFU-ig_k_of9HA8H2wI6bWxe00bsmwl2_BHIIPyJZbHE5r0WWVrpmupATAtGMk4dKqnBpHoRxrR3i49M'  # Replace this
+server_key = 'BH29N_jZscPwodEIDT0aC7o9l_WGmepP5cS31BViAYrG0xGaPrryFpA_zx7z0eLnJc1zTs0FihVttH2a3J5AihM'  # Replace this
 device_token = 'fTq9Zc1xBkg:APA91bFvS4-v5qXB_Hf...qABsM-dtNz'   # Replace this
 title = 'Hello from Python!'
 message = 'This is a test push notification.'
